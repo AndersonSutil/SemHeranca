4281f2783632f21f4e71f3f8eada7a5fb89e2cc2
1744952 <andersonsutil.off@gmail.com>
2020-01-10 19:12:49 -0300

    Testando exeções

--- src/ProcessingExceptions/ExceptionsMain.java
+++ src/ProcessingExceptions/ExceptionsMain.java
@@ -1,4 +1,35 @@
 package ProcessingExceptions;
 
 public class ExceptionsMain {
+    public static void main(String[] args) {
+
+        System.out.println("Inicio main");
+        m1();
+        System.out.println("Fim main");
+    }
+
+    public static void m1() {
+        System.out.println("Inicio m1");
+        m2();
+        System.out.println("Fim m1");
+    }
+
+    /**
+     * Metodo que possui o try/catch trata exceptions
+     */
+    public static void m2() {
+        System.out.println("Inicio m2");
+        try {
+
+//          int num = 3 / 0;
+            ContNull cont = null;
+
+        } catch (ArithmeticException | NullPointerException ex) {
+            System.out.println("Erro :" + ex.getMessage()+"\n");
+            System.out.println("Mostrando Pilha Afetada:\n");
+            ex.printStackTrace();
+            System.out.println("Fim Da Pilha >-< ");
+        }
+        System.out.println("Fim m2");
+    }
 }
