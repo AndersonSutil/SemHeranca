72e1aee004011cd46cfdcf9468c68beaec4dba86
1744952 <andersonsutil.off@gmail.com>
2020-01-24 09:43:59 -0300

    Utilizando Serializacao de Objeto Leitura/Escrita com serialVersionUID Com Marcação

--- src/Serializacao/SerializacaoObjeto.java
+++ src/Serializacao/SerializacaoObjeto.java
@@ -3,9 +3,15 @@ package Serializacao;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class SerializacaoObjeto  {
+public class SerializacaoObjeto implements Serializable {
+    //Marcarção de Objeto, Quando se a mudanças no Objeto,
+    // haverá uma comparação de marcação, caso sejá incompatíveis, joga uma exception na pilha
+    // private static final long serialVersionUID = 12L;
+
     public static void main(String[] args) throws IOException {
+
         String nome = "Anderson Sutil";
         //Transformando objeto em binario
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Objeto.bin"));
